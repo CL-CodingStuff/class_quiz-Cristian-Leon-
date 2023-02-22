46cc5b26094101cca680d3ee5084e612f978f2ad
CL-CodingStuff <121184311+CL-CodingStuff@users.noreply.github.com>
2023-02-22 16:28:41 +0000

    Test complete

--- card.py
+++ card.py
@@ -1,6 +1,20 @@
-# Name:
+# Name:Cristian Leon
 
 class Card:
-    """class representing a simple Neapolitan card. Holds a 
-    value and a suit"""
+    def __init__(self, value, suit):
+        self.suit = suit
+        self.value = value
+    
+    def __repr__(self):
+        return f"Card(value={self.value},suit={self.suit})"
+
+    def __str__(self):
+        return f"{self.value} of {self.suit}"
+    
+    def __eq__(self, other):
+        if self.value == other.value:
+            return True
+        else:
+            return False
+    
     
